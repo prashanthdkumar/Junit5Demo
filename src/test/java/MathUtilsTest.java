f1355ad6343f3f723c0342d5b3a30642ab9c6bac
Prashanth <prashanthdkumar88@gmail.com>
2021-12-19 01:02:57 -0500

    TDD approach for computeCircleArea method

--- src/test/java/MathUtilsTest.java
+++ src/test/java/MathUtilsTest.java
@@ -5,10 +5,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MathUtilsTest {
 
     @Test
-    void test() {
+    void testAdd() {
         MathUtils mathUtils = new MathUtils();
         int expected = 2;
         int actual = mathUtils.add(1, 1);
         assertEquals(expected, actual, "The Add method should add two numbers.");
+    }
+
+    @Test
+    void testComputeCircleArea() {
+        MathUtils mathUtils = new MathUtils();
+        assertEquals(314.1592653589793, mathUtils.computeCircleArea(10.0), "Should return right circle area");
     }
 }
