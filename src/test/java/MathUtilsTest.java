2841929e07885d4e03bacaca0a98a2400da165f0
Prashanth <prashanthdkumar88@gmail.com>
2021-12-19 00:20:50 -0500

    add

---
+++ src/test/java/MathUtilsTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MathUtilsTest {
+
+    @Test
+    void test() {
+        //assertTrue(true);
+        System.out.println("Test completed successfully");
+
+    }
+}
