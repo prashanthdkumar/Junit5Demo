c760ff5864fc7b124d1c3438eb7fb80a2f856474
Prashanth <prashanthdkumar88@gmail.com>
2021-12-19 01:31:43 -0500

    test for divide method

--- src/main/java/MathUtils.java
+++ src/main/java/MathUtils.java
@@ -4,6 +4,17 @@ public class MathUtils {
         return a + b;
     }
 
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
     public double computeCircleArea(double radius) {
         return Math.PI * radius * radius;
     }
--- src/test/java/MathUtilsTest.java
+++ src/test/java/MathUtilsTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MathUtilsTest {
 
@@ -10,6 +11,12 @@ class MathUtilsTest {
         int expected = 2;
         int actual = mathUtils.add(1, 1);
         assertEquals(expected, actual, "The Add method should add two numbers.");
+    }
+
+    @Test
+    void testDivide() {
+        MathUtils mathUtils = new MathUtils();
+        assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "The Divide by zero should throw");
     }
 
     @Test
